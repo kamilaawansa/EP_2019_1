772cc58c4841966bfdc444ddc7385a4990e44a12
ALuno B <mvcavalieri@hotmail.com>
2019-04-12 12:47:40 -0300

    Tarefa aluno B

--- ep1.py
+++ ep1.py
@@ -75,6 +75,8 @@ def main():
 
             # Aluno B: substitua este comentário e a linha abaixo pelo código
             # para pedir a escolha do usuário.
+            for opcao,valor in opcoes.items():
+                print('{0}:{1}'.format(opcao,valor))
             escolha = ""
 
             if escolha in opcoes:
